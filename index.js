dc1994eea6c407e439362a42161dea9a00804fbf
Kyler Gibbs <kylergibbs@hotmail.com>
2024-06-21 23:32:26 -0700

    Submit working.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Vars
-var cityName = "London";
 const key = "901280234eaf7bf09a3fe10472e067e6";
-// let lat
-// let lon
-// const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
-var city = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+var submit = document.querySelector('#form');
+var query = document.querySelector('#city');
 
 
 
-const getWeather = async () => {
+const getWeather = async (event) => {
+    event.preventDefault();
+    console.log("runzorz", event)
+    const cityName = query.value;
+    var city = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`
     const response = await fetch(city);
     const weather = await response.json();
     const lat = weather.coord.lat;
     const lon = weather.coord.lon;
-    const api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`
+    const api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=5&appid=${key}`
 
     const fiveDay = await fetch(api)
     const data = await fiveDay.json();
     console.log(data)
   }
 
-
-// getWeather();
+submit.addEventListener('submit', getWeather)
